53a1820fc10cf90c23b7c821add226e1fdd636ce
“Marksoloff” <marksoloff@gmail.com>
2018-08-24 15:09:26 -0500

    complete reddit_app exercise

--- reddit_app.rb
+++ reddit_app.rb
@@ -2,29 +2,7 @@ require 'http'
 
 response = HTTP.get("https://www.reddit.com/r/programming.json")
 
-
-# 2. list all posts for programming subreddit
-# 3. make it dynamic by asking the user to enter a name for a subreddit
-#   3a. If the subreddit exists, display all the posts
-#   3b. If the subreddit doesn’t exist or there is an error, display an error message
-
-# Bonus: After displaying all the posts from a subreddit, allow the user to enter a number to see the comments for a given post. You’ll need to make a second web request to get the comments!
-
-#if response.code == 404 ...........
-
-# puts response.body.readpartial  
-
 data = response.parse
-
-# post = data["data"]["children"][0]["data"]["title"]
-
-# puts children
-
-# children_2 = data["data"]["children"][1]["data"]["title"]
-
-# puts children_2 
-
-# puts children_3 = data["data"]["children"][2]["data"]["title"]
 
 puts "Here is a list of all posts in the programming subreddit:"
 
@@ -36,24 +14,18 @@ subreddits.each do |subreddit|
 end
 
 puts "Please type a subreddit"
-
 answer = gets.chomp
-vanilla_response = HTTP.get("https://www.reddit.com/r/#{answer}.com")
 response = HTTP.get("https://www.reddit.com/r/#{answer}.json")
-all_data = response.parse
 
-if vanilla_response.code == false
-  "Error! That subreddit doesn't exist!"
-else 
+if response.code == 404
+  puts "Error! That subreddit doesn't exist!"
+elsif response.code == 302
+  puts "302 error! Sorry buddy."
+else
+  all_data = response.parse
   subreddits = all_data["data"]["children"]
   subreddits.each do |subreddit|
     puts subreddit["data"]["title"]
     puts " " 
   end
 end
-
-
-
-# (1..100).each do |k|
-#    puts "#{k}. This is Ruby preferred way of doing loops, when possible"
-# end                  
